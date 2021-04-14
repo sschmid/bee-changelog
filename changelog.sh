a03b4cacf5895b45499b0379590d088ae798d572
Simon Schmid <mail@sschmid.com>
2021-04-14 13:52:30 +0200

    Insert changes into existing changelog and add markdown link

--- changelog.sh
+++ changelog.sh
@@ -3,7 +3,8 @@
 changelog::_new() {
   echo "# changelog => $(changelog::_deps)"
   echo 'CHANGELOG_PATH=CHANGELOG.md
-CHANGELOG_CHANGES=CHANGES.md'
+CHANGELOG_CHANGES=CHANGES.md
+CHANGELOG_URL=https://github.com/sschmid/bee-changelog'
 }
 
 changelog::_deps() {
@@ -12,12 +13,26 @@ changelog::_deps() {
 
 changelog::merge() {
   assert_file CHANGELOG_CHANGES
-  [[ ! -f "${CHANGELOG_PATH}" ]] && touch "${CHANGELOG_PATH}"
-  local tmp="${CHANGELOG_PATH}.tmp" version
+  local insert_changes_pattern="## \[Unreleased\]" insert_link_pattern="\[Unreleased\]:" tmp_changes="${CHANGELOG_CHANGES}.tmp" tmp_link="link.tmp" prev_version version
+  prev_version=$(grep --color=never "\[Unreleased\]:" CHANGELOG.md | grep -o -E --color=never "\d+\.\d+\.\d+")
   version="$(version::read)"
-  echo "## [${version}] - $(date +%Y-%m-%d)" > "${tmp}"
-  cat "${CHANGELOG_CHANGES}" >> "${tmp}"
-  echo >> "${tmp}"
-  cat "${CHANGELOG_PATH}" >> "${tmp}"
-  mv "${tmp}" "${CHANGELOG_PATH}"
+
+  {
+    echo
+    echo "## [${version}] - $(date +%Y-%m-%d)"
+    cat "${CHANGELOG_CHANGES}"
+  } > "${tmp_changes}"
+
+  {
+    echo "[Unreleased]: ${CHANGELOG_URL}/compare/${version}...HEAD"
+    echo "[${version}]: ${CHANGELOG_URL}/compare/${prev_version}...${version}"
+  } > "${tmp_link}"
+
+  sed -i .bak \
+    -e "/${insert_changes_pattern}/r ${tmp_changes}" \
+    -e "/${insert_link_pattern}.*/r ${tmp_link}" \
+    -e "/${insert_link_pattern}/d" \
+    ${CHANGELOG_PATH}
+
+  rm "${CHANGELOG_PATH}.bak" "${tmp_changes}" "${tmp_link}"
 }
