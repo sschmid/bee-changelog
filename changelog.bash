dbef6f8285e07df17d5b44f5169a227affddb1c7
Simon Schmid <mail@sschmid.com>
2021-12-14 00:25:19 +0100

    Update plugin according to bee 1.0.0 specs

--- changelog.bash
+++ changelog.bash
@@ -1,35 +1,44 @@
-#!/usr/bin/env bash
-
-changelog::_new() {
-  echo "# changelog => $(changelog::_deps)"
-  echo 'CHANGELOG_PATH=CHANGELOG.md
-CHANGELOG_CHANGES=CHANGES.md
+changelog::help() {
+  echo "# changelog => $(changelog::deps | xargs) - https://github.com/sschmid/bee-changelog"
+  cat << 'EOF'
+CHANGELOG_PATH=CHANGELOG.md # default
+CHANGELOG_CHANGES=CHANGES.md # default
 CHANGELOG_URL=https://github.com/sschmid/bee-changelog
-CHANGELOG_TAG_PREFIX=""
-CHANGELOG_TAG_SUFFIX=""'
+CHANGELOG_TAG_PREFIX="" # default
+CHANGELOG_TAG_SUFFIX="" # default
+EOF
 }
 
-changelog::_deps() {
+: "${CHANGELOG_PATH:=CHANGELOG.md}"
+: "${CHANGELOG_CHANGES:=CHANGES.md}"
+: "${CHANGELOG_TAG_PREFIX:=""}"
+: "${CHANGELOG_TAG_SUFFIX:=""}"
+
+changelog::deps() {
   echo "version"
 }
 
 changelog::merge() {
-  assert_file CHANGELOG_CHANGES
+  if [[ ! -f "${CHANGELOG_CHANGES}" ]]; then
+    bee::log_error "${CHANGELOG_CHANGES} not found!"
+    exit 1
+  fi
+
   local insert_changes_pattern="## \[Unreleased\]" insert_link_pattern="\[Unreleased\]:"
   local tmp_changes="${CHANGELOG_CHANGES}.tmp" tmp_link="link.tmp" prev_version version
-  prev_version=$(grep --color=never "\[Unreleased\]:" CHANGELOG.md | grep -o -E --color=never "\d+\.\d+\.\d+")
+  prev_version=$(grep --color=never "\[Unreleased\]:" "${CHANGELOG_PATH}" | grep -o -E --color=never "\d+\.\d+\.\d+")
   version="$(version::read)"
 
-  {
-    echo
-    echo "## [${version}] - $(date +%Y-%m-%d)"
-    cat "${CHANGELOG_CHANGES}"
-  } > "${tmp_changes}"
+  cat << EOF > "${tmp_changes}"
 
-  {
-    echo "[Unreleased]: ${CHANGELOG_URL}/compare/${CHANGELOG_TAG_PREFIX}${version}${CHANGELOG_TAG_SUFFIX}...HEAD"
-    echo "[${version}]: ${CHANGELOG_URL}/compare/${CHANGELOG_TAG_PREFIX}${prev_version}${CHANGELOG_TAG_SUFFIX}...${CHANGELOG_TAG_PREFIX}${version}${CHANGELOG_TAG_SUFFIX}"
-  } > "${tmp_link}"
+## [${version}] - $(date +%Y-%m-%d)
+$(< "${CHANGELOG_CHANGES}")
+EOF
+
+  cat << EOF > "${tmp_link}"
+[Unreleased]: ${CHANGELOG_URL}/compare/${CHANGELOG_TAG_PREFIX}${version}${CHANGELOG_TAG_SUFFIX}...HEAD
+[${version}]: ${CHANGELOG_URL}/compare/${CHANGELOG_TAG_PREFIX}${prev_version}${CHANGELOG_TAG_SUFFIX}...${CHANGELOG_TAG_PREFIX}${version}${CHANGELOG_TAG_SUFFIX}
+EOF
 
   sed -i .bak \
     -e "/${insert_changes_pattern}/r ${tmp_changes}" \
